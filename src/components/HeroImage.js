15734a4b03ed9543a70e6468bac5d9967d89608d
imranyy <imranmatano6@gmail.com>
2022-11-21 21:57:27 +0300

    almost done with about us section

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -7,10 +7,10 @@ function HeroImage(props) {
             <div className='Hero-image'>
             {/* <Navbar/> */}
                 <div className='hero-text' style={{marginTop:'300px',marginLeft:'150px'}}>
-                    <h1 className='Enhancing' style={{fontSize:'40px',fontWeight:'bolder'}}>
+                    <h1 className='Enhancing' style={{fontSize:'40px',fontWeight:'bolder',color:'#1E1E1E'}}>
                     Enhancing Brands, Enhancing Lives
                     </h1>
-                    <p className='text' style={{marginTop:"20px",fontSize:'20px'}}>
+                    <p className='text' style={{marginTop:"20px",fontSize:'20px',color:'#1E1E1E'}}>
                         We focus on deep customer insights to provide growth.
                     </p>
                     <button style={{marginTop:"20px", color:'white', background:'red',width:'100px',height:'50px', border:'none'}}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import HeroImage from '../components/HeroImage';
+import rect2 from '../media/Rectangle 2.png';
+import rect3 from '../media/Rectangle 3.png';
 
 function Home(props) {
     return (
         <>
-            <HeroImage/>
+            {/* <HeroImage/> */}
+            <div className='about'>
+                <div className='grid-item'>
+                    <h2>ABOUT US</h2>
+                        <p>
+                            One thing that sets us apart from other digital marketing agencies is that we are mission-driven and deeply invested in our clients’ work. We believe that mission-driven companies and social enterprises play a significant role in supporting the economic development and sustainability of the country.<br/><br/>
+                            Because you focus on changing the world, you deserve the best digital marketing, branding, and PR team to help you achieve your goals and increase impact work. One that understands the importance of telling your brand story, how to say it, and where to direct it.
+                        </p><br/>
+                    <h3>Mission Statement!</h3>
+                    To contribute to client’s business growth through result oriented, innovative and creative strategies that will deliver the WOW factor.<br/><br/>
+
+                    <h3>Core Values</h3>
+                    <ul>
+                        <li>Professionalism</li>
+                        <li>Integrity</li>
+                        <li>Passion driven</li>
+                        <li>Authenticity</li>
+                    </ul>
+                </div>
+
+                <div className='grid-item2'>
+                    <div className='rectangle2'></div>
+                    <img className='rectangle3' src={rect2} alt="."/>
+                </div>
+            </div>
+
         </>
     );
 }
