3292631a8a67d29c9160ecb5955410edda6bfee1
Ijay Abby <inyangasi4@gmail.com>
2022-11-22 02:02:10 +0300

    Add font

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -7,8 +7,11 @@ function HeroImage(props) {
             <div className='Hero-image'>
             {/* <Navbar/> */}
                 <div className='hero-text' style={{marginLeft:'150px'}}>
-                    <h1 className='Enhancing' style={{fontSize:'40px',fontWeight:'bolder',color:'#1E1E1E'}}>
-                    Enhancing Brands, Enhancing Lives
+                    <h1 className='Enhancing heading' style={{fontSize:'60px',fontWeight:'normal',color:'#1E1E1E'}}>
+                    Enhancing Brands,
+                    </h1>
+                    <h1 className='Enhancing heading' style={{fontSize:'60px',fontWeight:'normal',color:'#1E1E1E'}}>
+                    Enhancing Lives
                     </h1>
                     <p className='text' style={{marginTop:"20px",fontSize:'20px',color:'#1E1E1E'}}>
                         We focus on deep customer insights to provide growth.
