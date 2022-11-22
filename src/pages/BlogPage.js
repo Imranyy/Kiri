7542dc0ea8276c47c1f3d3a1c3d835b2825de794
imranyy <imranmatano6@gmail.com>
2022-11-22 15:14:08 +0300

    started on blogPage

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -5,9 +5,12 @@ function BlogPage(props) {
     return (
         <>
             <div className='blogPage'>
+               <div style={{paddingTop:'20px',marginBottom:'20px'}}>
                 <Navbar/>
+               </div>
                     <div className='header'>
                         <h1>The art of Storytelling</h1>
+                        <button>Read Article</button>
                     </div>
                 <Footer/>
             </div>
