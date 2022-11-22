0ef7968ccfb31d99d06220372d73cbb1b83bcb61
imranyy <imranmatano6@gmail.com>
2022-11-22 13:31:37 +0300

    done with landing page

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -7,10 +7,10 @@ function HeroImage(props) {
             <Navbar/>
                 <div className='Hero-image'>
                     <div className='hero-text' style={{marginLeft:'150px'}}>
-                        <h1 className='Enhancing' style={{fontSize:'40px',fontWeight:'bolder',color:'#1E1E1E'}}>
+                        <h1 className='Enhancing' style={{fontSize:'40px',fontWeight:'bolder',color:'#1E1E1E',fontFamily:'League Spartan'}}>
                         Enhancing Brands, Enhancing Lives
                         </h1>
-                        <p className='text' style={{marginTop:"20px",fontSize:'20px',color:'#1E1E1E'}}>
+                        <p className='text' style={{marginTop:"20px",fontSize:'20px',color:'#1E1E1E',fontFamily:'League Spartan'}}>
                             We focus on deep customer insights to provide growth.
                         </p>
                         <button style={{marginTop:"20px", color:'white', background:'red',width:'100px',height:'50px', border:'none'}}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,26 +3,26 @@ import {Link} from "react-router-dom";
 function Navbar(props) {
     return (
         <>
-            <nav className="navbar">
+            <nav className="navbar" >
                 <div className="brand">
-                    <Link to='/' className="navbar-brand">Kiri</Link>
+                    <Link to='/' className="navbar-brand" style={{fontFamily:'League Spartan'}}>Kiri</Link>
                 </div>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
                         <li className="nav-item">
-                            <Link to='/' className="nav-link active" aria-current="page">Home</Link>
+                            <Link to='/' className="nav-link active" aria-current="page" style={{fontFamily:'League Spartan'}}>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/about' className="nav-link">About</Link>
+                            <Link to='/about' className="nav-link" style={{fontFamily:'League Spartan'}}>About</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/services' className="nav-link">Services</Link>
+                            <Link to='/services' className="nav-link" style={{fontFamily:'League Spartan'}}>Services</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/news' className="nav-link">News</Link>
+                            <Link to='/news' className="nav-link" style={{fontFamily:'League Spartan'}}>News</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/contacts' className="nav-link">Contacts</Link>
+                            <Link to='/contacts' className="nav-link" style={{fontFamily:'League Spartan'}}>Contacts</Link>
                         </li> 
                     </ul>
                 </div>
